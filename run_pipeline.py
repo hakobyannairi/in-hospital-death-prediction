86e53bc54dff4269070593ed8619e2601e0c24c8
hakobyannairi <hakobyannairi@gmail.com>
2023-04-23 19:08:58 +0400

    added run_pipeline.py

---
+++ run_pipeline.py
@@ -0,0 +1,17 @@
+from model import Model
+from preprocessor import Preprocessor
+
+class Pipeline:
+    def __init__(self,):
+        self.model = Model()
+        self.preprocessor = Preprocessor()
+
+    def run(self, X, test=False):
+        if test:
+            pass
+        # load preprocessor and model for testing
+        # save results to predictions.json file
+        else:
+            pass
+        # call preprocessor and model for training
+        # save preprocessor and model for future testing
