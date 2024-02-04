bad3d5eed0428d4ddeaa312a4d4698747c5a5644
hakobyannairi <hakobyannairi@gmail.com>
2024-02-04 17:00:13 +0400

    got rid of unnecessary prints

--- model.py
+++ model.py
@@ -1,16 +1,9 @@
 class Model:
     def __init__(self,):
-        print('change from local')
         pass
 
     def fit(self, X, y):
-        print('function for testing')
         pass
 
     def predict(self, X):
-        print('change from local')
-        print('remote changes refused')
-        pass
-
-    def function_from_remote(self):
         pass
